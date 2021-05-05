e6682d09eab96467a9f894fb95eb891f14b4faa8
gmiskos1 <gmiskos@gmail.com>
2021-05-05 08:25:48 +0300

    creating components

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipie } from '../recipie.model';
 
 @Component({
   selector: 'app-recipie-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipie-list.component.css']
 })
 export class RecipieListComponent implements OnInit {
-
+  recipies:Recipie[] = [
+    new Recipie('Testing Recipie', 'Just a simply test', 'https://snappygoat.com/b/8aeb0835d905c355fe8b623b012cdce231b44eb1'),
+    new Recipie('Testing Recipie', 'Just a simply test', 'https://snappygoat.com/b/8aeb0835d905c355fe8b623b012cdce231b44eb1')
+  ];
   constructor() { }
 
   ngOnInit(): void {
